9cb6f304557cf8fd0f87eab6afef85e354cf5b16
Maxwell Benjamin Presley <Maxwell.Presley@mds.ac.nz>
2022-03-12 23:33:47 +1300

    update

---
+++ Movie Ticket/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+	// Delcaring variables and setting the mto 0 
+
+	int n1 = 0;
+	char q1;
+	//asking user to give number 
+
+	cout << "Enter your age:  ";
+	cin >> n1;
+	cout << "Are you a student? (y/n): ";
+	cin >> q1;
+	//Doing the maths 
+	if (n1 < 16) or (q1 = "y")
+		cout << "the price of your ticket is $10 as you are either a student or under 16!";
+	else 
+		cout << "The price of your ticket is $15 as you are not a student or under 16!";
+
+
+	//giving answer 
+
+return(0);
+}
